e089546920e8542125685709ff5015197e3c478e
DominicWells <dominic.wells@fubra.com>
2018-05-25 20:29:45 +0100

    add working image upload to admin index page

--- application/controllers/Users/Admin.php
+++ application/controllers/Users/Admin.php
@@ -11,6 +11,18 @@ use core\View;
 class Admin extends \core\Controller implements Users
 {
     /**
+     * Error property to pass into view if the file submitted by the user is of an incorrect format
+     * @var string
+     */
+    private $filetype_error;
+
+    /**
+     * Property to inform view when the user has successfully uploaded a photo to their profile.
+     * @var
+     */
+    private $upload_success;
+
+    /**
      * Before Filter - called before an action method.
      * @return void
      */
@@ -59,15 +71,17 @@ class Admin extends \core\Controller implements Users
      *
      * @return void
      */
-    public function indexAction() {
-
-        View::renderTemplate("admin/index.html", array(
-            // No Parameters
-        ));
+    public function indexAction()
+    {
 
         if (isset($_POST['upload_profile_image'])) {
             $this->uploadImage();
         }
+
+        View::renderTemplate("admin/index.html", array(
+            "filetype_error" => $this->filetype_error,
+            "upload_success" => $this->upload_success
+        ));
     }
 
     /**
@@ -77,6 +91,26 @@ class Admin extends \core\Controller implements Users
      */
     protected function uploadImage()
     {
-        print_r($_REQUEST);
+
+        if ($_FILES) {
+
+            $accepted_extensions = array('jpeg','png','gif','jpg');
+
+            $path = $_FILES['image']['name'];
+            $ext = pathinfo($path,PATHINFO_EXTENSION);
+
+            if (!in_array($ext,$accepted_extensions)) {
+
+                // The User tried to upload an invalid file type.
+                $this->filetype_error = "You cannot upload invalid file types!!";
+            } else {
+
+                $name = $_FILES['image']['tmp_name'];
+                $new_path = $_SERVER['DOCUMENT_ROOT'] . '/images/Admin_Image' . "." . $ext;
+
+                move_uploaded_file($name, $new_path);
+                $this->upload_success = "Congratulations! You have successfully uploaded a profile picture.";
+            }
+        }
     }
 }
